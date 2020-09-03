c705799245e422c2d7bc1888aa2ddb897b8e0a7b
Erik-joh <erik.johannesson0@gmail.com>
2020-09-03 17:08:29 +0200

    point tests

--- lib/points.ex
+++ lib/points.ex
@@ -3,4 +3,31 @@ defmodule Tetris.Points do
     Enum.map(points, fn {dx, dy} -> {dx + x, dy + y} end)
   end
 
+  def transpose(points) do
+    Enum.map(points, fn {x, y} -> {y, x} end )
+  end
+
+  def mirror(points) do
+    Enum.map(points, fn {x, y} -> {5-x, y} end )
+  end
+
+  def flip(points) do
+    Enum.map(points, fn {x, y} -> {x, 5-y} end )
+  end
+
+  def rotate_90(points) do
+    points
+    |> transpose
+    |> mirror
+  end
+
+  def rotate(points, 0), do: points
+  def rotate(points, degrees) do
+    rotate(
+      rotate_90(points),
+      degrees - 90
+      )
+  end
+
+
 end
